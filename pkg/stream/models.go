1dc9b2a212b6af0300965a12fc1f618b5fe4662c
Jaz <ericvolp12@gmail.com>
2024-02-24 10:45:39 -0800

    Rename stuff

--- pkg/stream/models.go
+++ pkg/stream/models.go
@@ -7,13 +7,13 @@ import (
 )
 
 type Record struct {
-	ID        uint `gorm:"primarykey;index:idx_repo_id,priority:2,order:desc"`
+	ID        uint `gorm:"primarykey;index:idx_records_repo_id,priority:2,order:desc"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	FirehoseSeq int64  `gorm:"index"`
-	Repo        string `gorm:"index:idx_path;index:idx_repo_id,priority:1"`
+	Repo        string `gorm:"index:idx_path;index:idx_records_repo_id,priority:1"`
 	Collection  string `gorm:"index:idx_path"`
 	RKey        string `gorm:"index:idx_path"`
 	Action      string
@@ -21,13 +21,13 @@ type Record struct {
 }
 
 type Event struct {
-	ID        uint `gorm:"primarykey;index:idx_repo_id,priority:2,order:desc"`
+	ID        uint `gorm:"primarykey;index:idx_events_repo_id,priority:2,order:desc"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 
 	FirehoseSeq int64  `gorm:"primaryKey"`
-	Repo        string `gorm:"index;index:idx_repo_id,priority:1"`
+	Repo        string `gorm:"index;index:idx_events_repo_id,priority:1"`
 	EventType   string `gorm:"index"`
 	Error       string
 	Time        int64 `gorm:"index"`
