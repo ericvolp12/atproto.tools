6baa3110b53fde2989c4ff151bcec1288905e98f
Jaz <ericvolp12@gmail.com>
2024-02-27 17:29:12 -0800

    Coming soon

--- ts/atproto-tools/src/components/welcome/Welcome.tsx
+++ ts/atproto-tools/src/components/welcome/Welcome.tsx
@@ -9,7 +9,7 @@ const Welcome: FC<{}> = () => {
         <div className="bg-white mt-10">
             <div className="mx-auto max-w-7xl px-2 align-middle">
                 <h1 className="text-4xl font-bold">Welcome to AT Proto Tools</h1>
-                <p className="text-lg">This is a suite of tools to help you build and test AT Proto applications and infrastructure.</p>
+                <p className="text-lg">Coming Soon: a suite of tools to help you build and test AT Proto applications and infrastructure.</p>
             </div>
         </div >
     );
