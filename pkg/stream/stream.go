a7378ab99abf204fe2391f2ecfc20356622da189
Jaz <ericvolp12@gmail.com>
2024-09-04 20:52:23 -0700

    Increase PLC cache size

--- pkg/stream/stream.go
+++ pkg/stream/stream.go
@@ -119,7 +119,7 @@ func NewStream(
 		SkipDNSDomainSuffixes: []string{".bsky.social"},
 	}
 
-	dir := identity.NewCacheDirectory(&base, 100_000, time.Hour*6, time.Minute*2, time.Hour*6)
+	dir := identity.NewCacheDirectory(&base, 500_000, time.Hour*6, time.Minute*2, time.Hour*6)
 
 	// Set pragmas for performance
 	writer.Exec("PRAGMA journal_mode=WAL;")
